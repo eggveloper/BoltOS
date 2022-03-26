9edb045082a2b6167ed2ed184cb36630160d308e
eggveloper <mhskripters@gmail.com>
2022-03-27 00:28:45 +0200

    [mem]: working implementation of frame allocation

--- src/core/debug.h
+++ src/core/debug.h
@@ -6,7 +6,7 @@
 #define DEBUG(format, ...) \
     serial_printf( \
         SERIAL_COM1, \
-        "[DEBUG]: %s:%d:%s(): " format, \
+        "[DEBUG]: %s:%d:%s(): " format "\n", \
         __FILE__, __LINE__, __func__, __VA_ARGS__ \
     )
 
--- src/kernel/kmain.c
+++ src/kernel/kmain.c
@@ -32,7 +32,7 @@ void kmain(unsigned long magic, unsigned long addr) {
     // Enable serial port
     serial_init(SERIAL_COM1, SERIAL_SPEED_115200);
 
-    DEBUG("%s has started!\n", KERNEL_NAME);
+    DEBUG("%s has started!", KERNEL_NAME);
 
     keyboard_init();
 
@@ -42,9 +42,13 @@ void kmain(unsigned long magic, unsigned long addr) {
     multiboot_tag_mmap_t* mmap = find_multiboot_tag(addr, MULTIBOOT_TAG_TYPE_MMAP);
     mmap_init(mmap, addr, (addr + *(unsigned *)addr));
 
-    uint32_t new_frame = mmap_allocate_frame();
-    uint32_t new_frame_addr = mmap_read(new_frame, MMAP_GET_ADDR);
-    printf("New frame allocated at: 0x%x\n", new_frame_addr);
+    uint32_t f1 = mmap_allocate_frame();
+    physical_address_t f1_addr = mmap_read(f1, MMAP_GET_ADDR);
+    printf("New frame allocated at: 0x%x\n", f1_addr);
+
+    uint32_t f2 = mmap_allocate_frame();
+    physical_address_t f2_addr = mmap_read(f2, MMAP_GET_ADDR);
+    printf("New frame allocated at: 0x%x\n", f2_addr);
 
     while (1) {
         __asm__("hlt");
