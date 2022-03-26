487e621d62003b5610d33d84c8a551fe7a9bd7d5
eggveloper <mhskripters@gmail.com>
2022-03-26 13:15:08 +0200

    [boot]: long mode

--- boot.asm
+++ boot.asm
@@ -12,54 +12,47 @@ start:
 
     mov ecx, 0
 
-    .map_p2_table:
-        mov eax, 0x200000
-        mul ecx
-        or eax, 0b10000011
-        mov [p2_table + ecx * 8], eax
+.map_p2_table:
+    mov eax, 0x200000
+    mul ecx
+    or eax, 0b10000011
+    mov [p2_table + ecx * 8], eax
 
-        inc ecx
-        cmp ecx, 512
-        jne .map_p2_table
+    inc ecx
+    cmp ecx, 512
+    jne .map_p2_table
 
-        ; Move page table address to cr3
-        mov eax, p4_table
-        mov cr3, eax
+    ; Move page table address to cr3
+    mov eax, p4_table
+    mov cr3, eax
 
-        ; Enable PAE
-        mov eax, cr4
-        or eax, 1 << 5
-        mov cr4, eax
+    ; Enable PAE
+    mov eax, cr4
+    or eax, 1 << 5
+    mov cr4, eax
 
-        ; Set long mode bit
-        mov ecx, 0xC0000080
-        rdmsr
-        or eax, 1 << 8
-        wrmsr
+    ; Set long mode bit
+    mov ecx, 0xC0000080
+    rdmsr
+    or eax, 1 << 8
+    wrmsr
 
-        ; Enable paging
-        mov eax, cr0
-        or eax, 1 << 31
-        or eax, 1 << 16
-        mov cr0, eax
+    ; Enable paging
+    mov eax, cr0
+    or eax, 1 << 31
+    or eax, 1 << 16
+    mov cr0, eax
 
-        ;   size place      thing
-        ;   |    |          |
-        ;   V    V          V
-        mov word [0xb8000], 0x0248 ; H
-        mov word [0xb8002], 0x0265 ; e
-        mov word [0xb8004], 0x026c ; l
-        mov word [0xb8006], 0x026c ; l
-        mov word [0xb8008], 0x026f ; o
-        mov word [0xb800a], 0x022c ; ,
-        mov word [0xb800c], 0x0220 ;
-        mov word [0xb800e], 0x0277 ; w
-        mov word [0xb8010], 0x026f ; o
-        mov word [0xb8012], 0x0272 ; r
-        mov word [0xb8014], 0x026c ; l
-        mov word [0xb8016], 0x0264 ; d
-        mov word [0xb8018], 0x0221 ; !
-        hlt
+    lgdt [gdt64.pointer]
+
+    ; Update selectors
+    mov ax, gdt64.data
+    mov ss, ax
+    mov ds, ax
+    mov es, ax
+
+    ; Jump to long mode
+    jmp gdt64.code:long_mode_start
 
 section .bss
 align 4096
@@ -83,3 +76,16 @@ gdt64:
 
 .data: equ $ - gdt64
     dq (1 << 44) | (1 << 47) | (1 << 41)
+
+.pointer:
+    dw .pointer - gdt64 - 1
+    dq gdt64
+
+section .text
+bits 64
+
+long_mode_start:
+    mov rax, 0x2f592f412f4b2f4f
+    mov qword [0xb8000], rax
+
+    hlt
